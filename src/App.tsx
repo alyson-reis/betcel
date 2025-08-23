69730ddfe0aa2e943d99ec0fb1e7c9d9b97cff04
alyson-reis <alysonandrade.2005@gmail.com>
2025-08-23 19:09:32 -0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import './App.css'
-import CallToActionBanner from './componets/CallToActionBanner'
-import Footer from './componets/Footer'
-import Header from './componets/Header'
-import HeroSection from './componets/HeroSection'
-import LocationSection from './componets/LocationSection'
-import ServicesSection from './componets/ServicesSection'
-import WhyChooseUsSection from './componets/WhyChooseUsSection'
+import Home from './pages/Home'
+
 
 function App() {
 
   return (
     <div>
-      <Header/>
-      <HeroSection/>
-      <ServicesSection/>
-      <LocationSection/>
-      <WhyChooseUsSection/>
-      <CallToActionBanner/>
-      <Footer/>
+      <Home/>
     </div>
   )
 }
